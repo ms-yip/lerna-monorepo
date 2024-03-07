0174909e872039f6c9666695c39c35950a68bc87
Yvonne <>
2024-03-07 18:51:42 +0800

    feat: test commit lint 4

--- packages/components/lib/components.js
+++ packages/components/lib/components.js
@@ -5,7 +5,7 @@ module.exports = components;
 function components () {
   console.log('change1');
   console.log('change2');
-    console.log('change3');
+  console.log('change3');
 
   return 'Hello from components';
 }
